0815f1e90592ab202c99a25deea547aa2e7c96c3
Al McKinlay <github@10people.co.uk>
2020-07-03 12:37:21 +0100

    Add gordon cottage heading and fix a few layout issues

--- src/sections/feedback/feedbackcard.js
+++ src/sections/feedback/feedbackcard.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from '../../components/Card';
 
 export default ({feedback}) => (
-    <div className="flex-auto px-2 w-1/3">
+    <div className="flex-auto px-2 lg:w-1/3 sm:w-1/2">
     <Card className="mb-8 flex items-center">
         <p className="mt-4">
             {feedback}
