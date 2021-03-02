243cd30667a3907a53fdc66a6a78fcec3c0b12ef
Al McKinlay <github@10people.co.uk>
2021-03-02 20:29:11 +0000

    Add get involved page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,13 @@ import Layout from '../components/layout/Layout';
 import Intro from '../sections/intro'
 import Feedback from '../sections/feedback/index';
 import About from '../sections/about';
+import Involved from '../sections/get-involved';
 
 export default () => (
   <Layout>
     <Intro></Intro>
     <About></About>
+    <Involved></Involved>
     <Feedback></Feedback>
   </Layout>
 );
---
+++ src/sections/get-involved.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default () => (
+    <section id="getinvolved" className="pt-20 md:pt-40">
+        <div className="container mx-auto px-8 lg:flex">
+            <div className="text-center lg:text-left lg:w-1/2 mr-2 text-lg">
+                <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold leading-none">
+                    Get Involved
+                </h1>
+                <p className="mt-6 font-light">
+                Anyone is welcome to attend our meetings, which are held quarterly. We particularly would like to see past or present parents, who can add experience and invaluable advice, especially on specialist toys and equipment. We would also like to invite local residents who have some spare time and an interest in the centre and would like to meet new people.
+                </p>
+            </div>
+            <div className="text-center lg:text-left lg:w-1/2 ml-2 text-lg">
+                <h1 className="text-3xl lg:text-4xl xl:text-5xl font-bold leading-none">
+                Become a member
+                </h1>
+                <p className="mt-6 font-light">
+                We do not currently have a membership fee. As a member you will receive an invitation to our AGM and you are welcome to join us at our quarterly meetings throughout the year. These are generally evening meetings, held at the cottage. As well as dealing with the business side of things these meetings have evolved to provide an informal ‘parent support group’ where parents can have a chat and share ideas and experiences. Occassionally, we also invite speakers to come to speak to the group.
+                </p>
+                <p className="mt-6 font-light">
+                If you would like to join, please email us with your name, address, email address and telephone number. Please also advise for our records what your interest in 'Friends of Gordon Cottage' is. ie Parent/Professional/Local Resident etc.
+                </p>
+                <p className="mt-6 font-light">
+                As the ‘Friends of Gordon Cottage’ is a Scottish Charity, any donations are always gratefully received.
+                </p>
+            </div>
+        </div>
+    </section>
+);
